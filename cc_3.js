6d06a22d83a82138d3ed74bf9e49d792b3fa1300
mvivy3 <meredithyen@usf.edu>
2025-01-31 16:29:06 -0500

    Task 2 - Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -7,3 +7,11 @@ price.pop(); // Remove the last price from the array
 console.log("Updated price list", price); // Log the updated prices 
 
 
+
+// Task 2: Modifying Customer Orders
+let quantity = [100, 200, 300, 400, 500]; // Array of quantities
+quantity[2] +=5; // Increase the third order quantity by 5
+console.log("Updated Array", quantity); // Log the updated array
+
+let totalQuantity = quantity.reduce((total, amount) => total + amount, 0); // Calculate the total number of all orders
+console.log(`Total Quantity: $${totalQuantity}`); // Log the total order count
