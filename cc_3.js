f00fccdad3ded623646eca84c043cde086093a2f
mvivy3 <meredithyen@usf.edu>
2025-01-31 16:28:31 -0500

    Task 1 - Product Price Management

--- cc_3.js
+++ cc_3.js
@@ -1,4 +1,9 @@
+// Task 1: Product Price Management
+let price  = [3400, 6800, 2300, 1500, 5000]; // Array of prices
 
+price.push(9698); // Add a new price to the end of the array
+price.pop(); // Remove the last price from the array
 
+console.log("Updated price list", price); // Log the updated prices 
 
 
